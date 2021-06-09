43e6f1459d7d013533610f7c78e05623c5a923f2
Kim Nes Leirvik <kim.leirvik@niva.no>
2021-06-09 13:29:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nortide',
-    version='0.1',
+    version='0.2',
     description='Python module for interfacing with the http://api.sehavniva.no/ API',
     long_description=long_description,
     url='https://github.com/NIVANorge/nortide',
